dc5471475aaa4ba5fc1c26e74ff1b12f6881511a
Rupak <rupakdey1998@gmail.com>
2021-12-05 23:34:41 +0530

    add env file

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 // API Key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyCRv_O2agtDvW97-36smZq2fDlK_-V2fUE";
+export const API_KEY = process.env.API_KEY;
 
 // Context Key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "f054ef5bc3387c963";
+export const CONTEXT_KEY = process.env.CONTEXT_KEY;
