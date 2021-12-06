4bdcbb27d5b6d90578fca61a16ac644c02cbc6d9
Rupak <rupakdey1998@gmail.com>
2021-12-06 15:42:13 +0530

    Add styles

--- pages/index.js
+++ pages/index.js
@@ -56,13 +56,13 @@ export default function Home() {
       </header>
 
       {/* Body */}
-      <form className="flex flex-col mt-28 items-center flex-grow w-full">
+      <form className="flex flex-col mt-40 items-center flex-grow w-full">
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           height={100}
           width={300}
         />
-        <div className="flex w-4/5 mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+        <div className="flex w-4/5 mt-10 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500" />
           <input
             ref={searchInputRef}
